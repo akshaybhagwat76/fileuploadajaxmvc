76c3258011c6a3c97b8a1c2193b89b9d8b83d8bc
Akshay Bhagwat <akshaybhagwat76@gmail.com>
2017-08-07 09:07:55 +0530

    image

---
+++ FileUploadMVCAJAX/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using FileUploadMVCAJAX.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadMVCAJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult ImageUpload(ImageModal img)
+        {
+            var file = img.imageFile;
+            if (file!=null)
+            {
+                var fname = Path.GetFileName(file.FileName);
+                var ext = Path.GetExtension(file.FileName);
+                var without = Path.GetFileNameWithoutExtension(file.FileName);
+                file.SaveAs(Server.MapPath("/imagefolder/" + file.FileName));
+            }
+            return Json(file.FileName, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ FileUploadMVCAJAX/Models/ImageModal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileUploadMVCAJAX.Models
+{
+    public class ImageModal
+    {
+        public HttpPostedFileWrapper imageFile { get; set; }
+    }
+}
